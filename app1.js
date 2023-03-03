b18d056c71551adbade90b56a495f1c759b4c6eb
pankaj511995 <kpankaj511995@gmail.com>
2023-03-03 14:51:35 +0530

    writing_file

--- app1.js
+++ app1.js
@@ -1,22 +1,37 @@
 const http=require('http')
+const fs=require('fs')
 const server=http.createServer((req,res)=>{
     const url=req.url
+    const method=req.method
+    console.log(method)
     res.setHeader('Content-type','text/html')
-    if(url==='/home'){
+    if(url==='/'){
+      const mess=fs.readFileSync('message.txt').toString()
        res.write(`<html>
-       <body> Welcome to Home page</body> </html>`)
+        
+       <body><div>${mess}</div> 
+       <form action="/message" method="POST">
+       <input type="text name="messages">
+       <button type="submit">Send</button>
+       </form> </html>`)
        res.end()
     }
-    if(url==='/about'){
-        res.write(`<html>
-        <body> Welcome to About Us page</body> </html>`)
-        res.end()
-     }
-     if(url==='/node'){
-        res.write(`<html>
-        <body>Welcome to my Node Js project</body> </html>`)
-        res.end()
-     }
-})
+    if(url==='/message' && method==='POST'){
+    
+      const body=[]
+      req.on('data',(chunk)=>body.push(chunk));
+       
+     return req.on('end',()=>{
+        
+         const bufferbody=Buffer.concat(body).toString()
+         const message=bufferbody.split('=')[1]
+         fs.writeFile('message.txt',message,err=>{
+            res.setHeader(302)
+            res.setHeader('Location','/')
+            res.end()
+         })
+      })
+   }
+ })
 
 server.listen(4000)
