52dd975b87c742f4de06d35969eed1d31a2b07da
pankaj511995 <kpankaj511995@gmail.com>
2023-03-03 11:26:10 +0530

    rout url

--- app1.js
+++ app1.js
@@ -1,6 +1,22 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
-    console.log('pankaj')
+    const url=req.url
+    res.setHeader('Content-type','text/html')
+    if(url==='/home'){
+       res.write(`<html>
+       <body> Welcome to Home page</body> </html>`)
+       res.end()
+    }
+    if(url==='/about'){
+        res.write(`<html>
+        <body> Welcome to About Us page</body> </html>`)
+        res.end()
+     }
+     if(url==='/node'){
+        res.write(`<html>
+        <body>Welcome to my Node Js project</body> </html>`)
+        res.end()
+     }
 })
 
 server.listen(4000)
